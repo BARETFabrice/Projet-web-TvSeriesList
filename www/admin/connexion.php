230cc4efcd5b421d927abb88554796fe5b8a0c69
fsimoneau <fsimoneau997@gmail.com>
2018-01-29 13:40:00 -0500

    Ajout des dossiers de base pour séparer le travail

---
+++ www/admin/connexion.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Connexion Administrateur</title>
+</head>
+<body>
+	Connexion Admin
+</body>
+</html>
---
+++ www/membre/connexion.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Connexion Membre</title>
+</head>
+<body>
+	Connexion Membre
+</body>
+</html>
