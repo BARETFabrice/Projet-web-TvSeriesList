389fd30496b5619b0c7129cbf6da5efd3ebea106
baretfabrice <baretfabrice974@gamil.com>
2018-01-29 17:00:03 -0500

    modif partiel index html

--- www/index.php
+++ www/index.php
@@ -11,85 +11,37 @@
 
 <body>
 <header>
+    <div class="top-bar">
+        <div class="top-bar-right">
+            <ul class="menu">
+                <li><input type="search" placeholder="Search"></li>
+                <li><button type="button" class="button">Search</button></li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="medium-4 columns">
+            <h1>TvSeriesList.net</h1>
+            <!--p>taille image 450x183</p-->
+        </div>
+        <div class="medium-8 columns">
+            
+        </div>
+    </div>
 
-<div class="top-bar">
-<div class="top-bar-left">
-<ul class="menu">
-<li><a href="#">One</a></li>
-<li><a href="#">Two</a></li>
-<li><a href="#">Three</a></li>
-<li><a href="#">Four</a></li>
-</ul>
-</div>
-<div class="top-bar-right">
-<ul class="menu">
-<li><input type="search" placeholder="Search"></li>
-<li><button type="button" class="button">Search</button></li>
-</ul>
-</div>
-</div>
-
-
-<div class="row">
-<div class="medium-4 columns">
-<img src="https://placehold.it/450x183&text=LOGO" alt="company logo">
-</div>
-<div class="medium-8 columns">
-<img src="https://placehold.it/900x175&text=Responsive Ads - ZURB Playground/333" alt="advertisement for deep fried Twinkies">
-</div>
-</div>
-
-<br>
-<div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
-<button class="menu-icon" type="button" data-toggle></button>
-<div class="title-bar-title">Menu</div>
-</div>
-<div class="top-bar" id="main-menu">
-<ul class="menu vertical medium-horizontal expanded medium-text-center" data-responsive-menu="drilldown medium-dropdown">
-<li class="has-submenu"><a href="#">Tech</a>
-<ul class="submenu menu vertical" data-submenu>
-<li><a href="#">One</a></li>
-<li><a href="#">Two</a></li>
-<li><a href="#">Three</a></li>
-</ul>
-</li>
-<li class="has-submenu"><a href="#">Energy</a>
-<ul class="submenu menu vertical" data-submenu>
-<li><a href="#">One</a></li>
-<li><a href="#">Two</a></li>
-<li><a href="#">Three</a></li>
-</ul>
-</li>
-<li class="has-submenu"><a href="#">Space</a>
-<ul class="submenu menu vertical" data-submenu>
-<li><a href="#">One</a></li>
-<li><a href="#">Two</a></li>
-<li><a href="#">Three</a></li>
-</ul>
-</li>
-<li class="has-submenu"><a href="#">Medicine</a>
-<ul class="submenu menu vertical" data-submenu>
-<li><a href="#">One</a></li>
-<li><a href="#">Two</a></li>
-<li><a href="#">Three</a></li>
-</ul>
-</li>
-<li class="has-submenu"><a href="#">Robotics</a>
-<ul class="submenu menu vertical" data-submenu>
-<li><a href="#">One</a></li>
-<li><a href="#">Two</a></li>
-<li><a href="#">Three</a></li>
-</ul>
-</li>
-<li class="has-submenu"><a href="#">Tesla</a>
-<ul class="submenu menu vertical" data-submenu>
-<li><a href="#">One</a></li>
-<li><a href="#">Two</a></li>
-<li><a href="#">Three</a></li>
-</ul>
-</li>
-</ul>
-</div>
+    <br>
+    <div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
+        <button class="menu-icon" type="button" data-toggle></button>
+        <div class="title-bar-title">Menu</div>
+    </div>
+    <div class="top-bar" id="main-menu">
+        <ul class="menu vertical medium-horizontal expanded medium-text-center" data-responsive-menu="drilldown medium-dropdown">
+            <li class="has-menu"><a href="#">Accueil</a></li>
+            <li class="has-menu"><a href="#">Seriés</a></li>
+            <li class="has-menu"><a href="#">Nouveauté</a></li>
+            <li class="has-menu"><a href="#">Top</a></li>
+        </ul>
+    </div>
 </header>
 <br>
 <div class="row">
@@ -103,7 +55,7 @@
 </div>
 <hr>
 <div class="row column">
-<h4 style="margin: 0;" class="text-center">BREAKING NEWS</h4>
+<h4 style="margin: 0;" class="text-center">LES SÉRIES DU MOMENT</h4>
 </div>
 <hr>
 <div class="row small-up-3 medium-up-4 large-up-5">
@@ -124,9 +76,6 @@
 </div>
 </div>
 <hr>
-<div class="row column">
-<h4 style="margin: 0;" class="text-center">LATEST STORIES</h4>
-</div>
 <hr>
 <div class="row">
 <div class="large-8 columns" style="border-right: 1px solid #E3E5E8;">
