440987588b21cf56dc5c4a7802ebf376427f6180
fsimoneau <fsimoneau997@gmail.com>
2018-01-31 09:05:30 -0500

    Fix CSS pour les forms de connexion

--- www/prive/admin/connexion.php
+++ www/prive/admin/connexion.php
@@ -1,17 +1,22 @@
 <?php
 include "page/page-header.php";
 ?>
-<form class="log-in-form">
-  <h4 class="text-center">Se connecter en tant qu'admin</h4>
-  <label>Courriel
-    <input type="email" placeholder="somebody@example.com">
-  </label>
-  <label>Mot de passe
-    <input type="password" placeholder="Password">
-  </label>
-  <p><input type="submit" class="button expanded" value="Log in"></input></p>
-  <p class="text-center"><a href="#">Vous avez oublié votre mot de passe ?</a></p>
-</form>
+<div class="row column align-center medium-6 large-4 container-padded div_login">
+	<form class="log-in-form">
+	  <h4 class="text-center">Se connecter en tant qu'admin</h4>
+	  <label>Courriel
+		<input type="email" placeholder="somebody@example.com">
+	  </label>
+	  <label>Mot de passe
+		<input type="password" placeholder="Password">
+	  </label>
+	  <p><input type="submit" class="button expanded" value="Log in"></input></p>
+	  <p class="text-center"><a href="#">Vous avez oublié votre mot de passe ?</a></p>
+	</form>
+</div>
 <?php
 include "page/page-footer.php";
 ?>
+
+
+
