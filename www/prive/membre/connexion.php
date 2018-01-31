90ca935adc760de3c5fe9eb88e16cf8bdf59c51d
alex007sirois <alex007sirois@gmail.com>
2018-01-31 09:14:28 -0500

    page connexion.php

--- www/prive/membre/connexion.php
+++ www/prive/membre/connexion.php
@@ -1,9 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Connexion Membre</title>
-</head>
-<body>
-	Connexion Membre
-</body>
-</html>
+<?php
+include "../../public/page/page-header.php";
+?>
+<div class="row column align-center medium-6 large-4 container-padded div_login">
+    <form class="log-in-form">
+      <h4 class="text-center">Se connecter en tant que membre</h4>
+      <label>Courriel
+        <input type="email" placeholder="somebody@example.com">
+      </label>
+      <label>Mot de passe
+        <input type="password" placeholder="Password">
+      </label>
+      <p><input type="submit" class="button expanded" value="Log in"></input></p>
+      <p class="text-center"><a href="#">Vous avez oublié votre mot de passe ?</a></p>
+    </form>
+</div>
+<?php
+include "../../public/page/page-footer.php";
+?>
