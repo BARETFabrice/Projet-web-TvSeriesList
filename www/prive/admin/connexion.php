898c03b01d8ba83712de98ab8cb52217d13e2514
fsimoneau <fsimoneau997@gmail.com>
2018-01-31 08:48:06 -0500

    Forms CSS pour la connexion

--- www/prive/admin/connexion.php
+++ www/prive/admin/connexion.php
@@ -1,9 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Connexion Administrateur</title>
-</head>
-<body>
-	Connexion Admin
-</body>
-</html>
+<?php
+include "page/page-header.php";
+?>
+<form class="log-in-form">
+  <h4 class="text-center">Se connecter en tant qu'admin</h4>
+  <label>Courriel
+    <input type="email" placeholder="somebody@example.com">
+  </label>
+  <label>Mot de passe
+    <input type="password" placeholder="Password">
+  </label>
+  <p><input type="submit" class="button expanded" value="Log in"></input></p>
+  <p class="text-center"><a href="#">Vous avez oublié votre mot de passe ?</a></p>
+</form>
+<?php
+include "page/page-footer.php";
+?>
