53fef1b4f8afcda8e08d226d49032acfa8fa58c1
fsimoneau <fsimoneau997@gmail.com>
2018-02-01 07:55:28 -0500

    Ajout de la page recherche de séries et la page détail série

---
+++ www/prive/admin/liste-series.php
@@ -0,0 +1,9 @@
+<?php
+include "page/page-header.php";
+?>
+    
+Liste des séries
+
+<?php
+include "page/page-footer.php";
+?>
---
+++ www/prive/admin/serie.php
@@ -0,0 +1,9 @@
+<?php
+include "page/page-header.php";
+?>
+    
+Rick and Morty
+
+<?php
+include "page/page-footer.php";
+?>
