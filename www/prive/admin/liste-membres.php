6805def2c3b2956c2981ecde1a84df3035f25974
fsimoneau <fsimoneau997@gmail.com>
2018-02-01 07:56:02 -0500

    Ajout de la page recherche membres et de la page détail membre

---
+++ www/prive/admin/liste-membres.php
@@ -0,0 +1,9 @@
+<?php
+include "page/page-header.php";
+?>
+    
+Liste des membres
+
+<?php
+include "page/page-footer.php";
+?>
---
+++ www/prive/admin/membre.php
@@ -0,0 +1,9 @@
+<?php
+include "page/page-header.php";
+?>
+    
+hunter2
+
+<?php
+include "page/page-footer.php";
+?>
