6bdd295834351fc7607c6db7b8de1ef64afabcf7
fsimoneau <fsimoneau997@gmail.com>
2018-02-05 13:34:52 -0500

    Ajout fonction recherche pour les séries

--- www/prive/admin/liste-series.php
+++ www/prive/admin/liste-series.php
@@ -1,8 +1,17 @@
 <?php
 include "page/page-header.php";
 ?>
-    
-Liste des séries
+
+<div class="row column align-center medium-6 large-4 container-padded div_login">
+    <form class="log-in-form">
+      <h4 class="text-center">Rechercher une série</h4>
+      
+      <hr>
+	  
+        <input type="text" placeholder="Exemple : Rick and Morty">
+      <p><input type="submit" class="button expanded" value="Rechercher"></input></p>
+    </form>
+</div>
 
 <?php
 include "page/page-footer.php";
