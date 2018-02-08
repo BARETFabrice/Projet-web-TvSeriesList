110398adfd72b8a250a2be6db89420447a70c215
alex007sirois <alex007sirois@gmail.com>
2018-02-08 09:47:37 -0500

    MembreDao est fonctionnel #12

--- www/dao/MembreDAO.php
+++ www/dao/MembreDAO.php
@@ -1,15 +1,39 @@
 <?php
 
-include 'ConnexionBaseDeDonnees.php';
-
-
 class MembreDAO
 {
-	var $nomTable='Membre';
+	private $nomTable='Membre';
+	private $connexion;
+	private $Membre;
 	
+	function __construct(){
+		require_once '../modele/Membre.php';
+		$this->Membre=$Membre;
+		
+		require_once 'ConnexionBaseDeDonnees.php';
+		$this->connexion=$connexion;
+	}
 	
+	 function __destruct() {
+        $this->connexion=null;
+	 }
+	
+	function getMembre($idMembre) {
+		
+		$this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "SELECT * FROM Membre WHERE idMembre=:idMembre";
+		$stmt = $this->connexion->prepare($sql); 
+		
+		$stmt->bindParam(':idMembre', $idMembre);
+		
+		$stmt->execute();
+		
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		print_r($result);
+	}
 }
 
-$connexion=null;
+$membreDAO = new MembreDAO();
+$membreDAO->getMembre(2);
 
 ?>
