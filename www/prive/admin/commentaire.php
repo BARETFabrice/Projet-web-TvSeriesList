68261741372df1f51bcf5eadee5f8288ec01b592
fsimoneau <fsimoneau997@gmail.com>
2018-02-08 13:53:11 -0500

    Administration d'un commentaire #10

---
+++ www/prive/admin/commentaire.php
@@ -0,0 +1,23 @@
+<?php
+include "page/page-header.php";
+?>
+
+<div class="row column align-center medium-6 large-4 container-padded div_login">
+    <form class="log-in-form">
+		<h4 class="text-center">Commentaire</h4>
+		<p>Par <a href="#">Fred</a></p>
+		<p>Article : <a href="#">Les meilleurs moments de LOST</a></p>
+		<hr>
+
+		<label>Contenu
+			<textarea rows="4" cols="50">Quel article incroyable! Félicitation!
+			</textarea>
+		</label>
+		<p><input type="submit" class="button expanded" value="Modifier"></input></p>
+		<p><input type="submit" class="button expanded alert" value="Supprimer"></input></p>
+    </form>
+</div>
+
+<?php
+include "page/page-footer.php";
+?>
