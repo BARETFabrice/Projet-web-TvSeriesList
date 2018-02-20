9bf8da72f30a045b46bce0aed6ea2a5e9ba3aa7e
fsimoneau <fsimoneau997@gmail.com>
2018-02-20 12:26:40 -0500

    DAO saison #20

--- www/dao/SaisonDAO.php
+++ www/dao/SaisonDAO.php
@@ -1,15 +1,83 @@
 <?php
-
-include 'ConnexionBaseDeDonnees.php';
-
+require_once $_SERVER['DOCUMENT_ROOT'] . "/modele/Saison.php";
 
 class SaisonDAO
 {
-	var $nomTable='Saison';
+	private static $_instance;
+	private static $_connexion;
 	
+	function __construct(){
+		require 'ConnexionBaseDeDonnees.php';
+		self::$_connexion=$connexion;
+	}
 	
+	function __destruct() {
+	$this->connexion=null;
+	}
+	
+	public static function getInstance()
+    {
+        if ((self::$_instance) == null) 
+        {
+            self::$_instance = new SaisonDAO();
+        }
+        return self::$_instance;
+    }
+	
+	function getSaison($idSaison)
+	{
+		self::$_connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = 'SELECT * FROM saison WHERE idSaison=:idSaison';
+		$stmt = self::$_connexion->prepare($sql); 
+		
+		$stmt->bindParam(':idSaison', $idSaison);
+		
+		$stmt->execute();
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		$saison = new Saison($result['idSaison'], $result['idSerie'], $result['numero'], $result['titre'], $result['titre_fr'], $result['image'], $result['fini']);
+		
+		return $saison;
+	}
+	
+	function ajouterSaison($saison){
+		$sql = "INSERT INTO saison (idSerie, numero, titre, titre_fr, image, fini)
+			VALUES (:idSerie,:numero,:titre,:titre_fr,:image,:fini)";
+		$stmt = self::$_connexion->prepare($sql); 
+		
+		$stmt->bindParam(':idSerie', $saison->getIdSerie());
+		$stmt->bindParam(':numero', $saison->getNumero());
+		$stmt->bindParam(':titre', $saison->getTitre());
+		$stmt->bindParam(':titre_fr', $saison->getTitre_fr());
+		$stmt->bindParam(':image', $saison->getImage());
+		$stmt->bindParam(':fini', $saison->isFini());
+		
+		$stmt->execute();
+		
+		$saison->setId(self::$_connexion->lastInsertId());
+	}
+	
+	function supprimerSaison($saison){
+		$sql = 'DELETE FROM saison WHERE idSaison=:idSaison';
+		$stmt = self::$_connexion->prepare($sql); 
+		$stmt->bindParam(':idSaison', $saison->getId());
+		$stmt->execute();
+	}
+	
+	function modifierSaison($saison){
+		$sql = 'UPDATE saison SET idSerie=:idSerie, numero=:numero, titre=:titre,
+			titre_fr=:titre_fr, image=:image, fini=:fini WHERE idSaison=:idSaison';
+		$stmt = self::$_connexion->prepare($sql); 
+		
+		$stmt->bindParam(':idSaison', $saison->getId());
+		$stmt->bindParam(':idSerie', $saison->getIdSerie());
+		$stmt->bindParam(':numero', $saison->getNumero());
+		$stmt->bindParam(':titre', $saison->getTitre());
+		$stmt->bindParam(':titre_fr', $saison->getTitre_fr());
+		$stmt->bindParam(':image', $saison->getImage());
+		$stmt->bindParam(':fini', $saison->isFini());
+		
+		$stmt->execute();
+	}
 }
-
-$connexion=null;
 
 ?>
