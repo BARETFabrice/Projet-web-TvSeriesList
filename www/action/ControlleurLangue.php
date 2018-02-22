922587eee1d2c118bfabbf011f46f2488724ed43
alex007sirois <alex007sirois@gmail.com>
2018-02-22 09:09:53 -0500

    fonction verifirier cookie #17

--- www/action/ControlleurLangue.php
+++ www/action/ControlleurLangue.php
@@ -2,17 +2,26 @@
 
 class ControlleurLangue
 {
-    
-    public static function getLangue()
+    private static function verifyLangue()
     {
         if(!isset($_COOKIE['langue']))
             setcookie('langue','en_CA', time()+100000000);
+    }
+    
+    public static function getLangue()
+    {
+        self::verifyLangue();
 	    return ($_COOKIE['langue']);
     }
     
     public static function setLangue($langue)
     {
-        setcookie('langue',$langue, time()+100000000);
+        self::verifyLangue();
+            
+        if($langue==='en_CA' || $langue==='fr_CA')
+            $_COOKIE['langue']=$langue;
+        
+        return $_COOKIE['langue'];
     }
     
 }
