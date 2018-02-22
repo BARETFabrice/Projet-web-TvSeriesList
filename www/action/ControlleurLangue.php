8d607fb206939c46f65addfb448447e17c8132fa
alex007sirois <alex007sirois@gmail.com>
2018-02-22 09:49:25 -0500

    fixé les cookies qui marchent mal

--- www/action/ControlleurLangue.php
+++ www/action/ControlleurLangue.php
@@ -5,22 +5,23 @@ class ControlleurLangue
     private static function verifyLangue()
     {
         if(!isset($_COOKIE['langue']))
-            setcookie('langue','en_CA', time()+100000000);
+            setcookie('langue','en_CA', time()+100000000, '/');
     }
     
     public static function getLangue()
     {
         self::verifyLangue();
-	    return ($_COOKIE['langue']);
+	    return $_COOKIE['langue'];
     }
     
     public static function setLangue($langue)
     {
         self::verifyLangue();
             
-        if($langue==='en_CA' || $langue==='fr_CA')
-            $_COOKIE['langue']=$langue;
-        
+        if($langue==='en_CA' || $langue==='fr_CA'){
+            setcookie('langue', $langue, time()+100000000,'/');
+            $_COOKIE['langue'] = $langue;
+        }
         return $_COOKIE['langue'];
     }
     
