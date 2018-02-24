95a12538e483cb3d1831174d896b75cbb3772a61
alex007sirois <alex007sirois@gmail.com>
2018-02-24 13:16:42 -0500

    refactoring inscription (2) #22

---
+++ www/prive/membre/fragmentInscriptionMotDePasse.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/action/controlleurInscription.php';
+$membre=controlleurInscription::getInstance()->getMembre();
+
+?>
+
+<form class="log-in-form" method="post" action="./inscription?etape=3">
+  <h4 class="text-center">Inscription membre</h4>
+  
+  <hr>
+  <label>Mot de passe
+    <input type="password" value="<?=$membre->getMotDePasse()?>" name="motDePasse" placeholder="Password" required>
+  </label>
+  <label>Confirmer Mot de passe
+    <input type="password" value="<?=$membre->getMotDePasse()?>" name="confirmerMotDePasse" placeholder="Password" required>
+  </label>
+  <p><input type="submit" name="etape2" class="button expanded" value="Sign up"></input></p>
+</form>
