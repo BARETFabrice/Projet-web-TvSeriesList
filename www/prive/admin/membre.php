b85b8a7fe6520c729f40c98484a3b7769785889f
alex007sirois <alex007sirois@gmail.com>
2018-02-26 14:21:07 -0500

    #25 modifier le membre

--- www/prive/admin/membre.php
+++ www/prive/admin/membre.php
@@ -13,22 +13,26 @@ if ($controlleur->estPasEnSuppression())
 ?>
 
 <div class="row column align-center medium-6 large-4 container-padded div_login">
-    <form class="log-in-form">
+    <form class="log-in-form" action="" method="post">
 		<h4 class="text-center">Membre</h4>
 		<hr>
 
 		<label>Pseudonyme
-			<input type="text" value="<?=$membre->getPseudonyme()?>">
+			<input type="text" name="pseudonyme" value="<?=$membre->getPseudonyme()?>">
 		</label>
 		<label>Courriel
-			<input type="text" value="<?=$membre->getCourriel()?>">
+			<input type="text" name="courriel" value="<?=$membre->getCourriel()?>">
 		</label>
+		<label>
+		    Mot de Passe
+		    <input id="motDePasseAjoutMembreAdmin" type="hidden" name="motDePasse" value="">
+		    <button onclick="this.style.visibility='hidden'; document.getElementById('motDePasseAjoutMembreAdmin').setAttribute('type', 'text');" type="button" class="button expanded secondary">Nouveau mot de passe</button>
+		</label>
+		<p><input type="checkbox" <?php if($membre->isNotification())echo'checked'; ?> name="notification">Notifications</p>
 		<p><input type="checkbox" <?php if($membre->isAuteur())echo'checked'; ?> name="auteur">Auteur</p>
 		<p><input type="checkbox" <?php if($membre->isModerateur())echo'checked'; ?> name="moderateur">Modérateur</p>
-		<p><input type="checkbox" <?php if($membre->isNotification())echo'checked'; ?> name="notification">Notifications</p>
-		<p><input type="submit" class="button expanded" value="Modifier"></input></p>
-		<p><input type="submit" class="button expanded alert" value="Supprimer"></input></p>
-		<p><input type="submit" class="button expanded secondary" value="Nouveau mot de passe"></input></p>
+		<p><input type="submit" class="button expanded" name="modifier" value="Modifier"></input></p>
+		<p><input type="submit" class="button expanded alert" name="supprimer" value="Supprimer"></input></p>
     </form>
 </div>
 
