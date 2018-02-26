b90d8c64142e706d987ec886dcd49e4bce2229de
alex007sirois <alex007sirois@gmail.com>
2018-02-26 12:12:25 -0500

    #25 page modifier membre

--- www/prive/admin/membre.php
+++ www/prive/admin/membre.php
@@ -1,21 +1,25 @@
 <?php
 include "fragmentHautPage.php";
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/action/controlleurPageMembre.php';
+$controlleur = controlleurPageMembre::getInstance();
+$membre = $controlleur->getMembre($_GET['id']);
 ?>
 
 <div class="row column align-center medium-6 large-4 container-padded div_login">
     <form class="log-in-form">
-		<h4 class="text-center">bob</h4>
+		<h4 class="text-center">Membre</h4>
 		<hr>
 
 		<label>Pseudonyme
-			<input type="text" value="bob">
+			<input type="text" value="<?=$membre->getPseudonyme()?>">
 		</label>
 		<label>Courriel
-			<input type="text" value="bob@bob.bob">
+			<input type="text" value="<?=$membre->getCourriel()?>">
 		</label>
-		<p><input type="checkbox" name="fini">Auteur</p>
-		<p><input type="checkbox" name="fini">Modérateur</p>
-		<p><input type="checkbox" name="fini">Notifications</p>
+		<p><input type="checkbox" <?php if($membre->isAuteur())echo'checked'; ?> name="auteur">Auteur</p>
+		<p><input type="checkbox" <?php if($membre->isModerateur())echo'checked'; ?> name="moderateur">Modérateur</p>
+		<p><input type="checkbox" <?php if($membre->isNotification())echo'checked'; ?> name="notification">Notifications</p>
 		<p><input type="submit" class="button expanded" value="Modifier"></input></p>
 		<p><input type="submit" class="button expanded alert" value="Supprimer"></input></p>
 		<p><input type="submit" class="button expanded secondary" value="Nouveau mot de passe"></input></p>
