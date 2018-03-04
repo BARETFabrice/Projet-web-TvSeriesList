e594bd16fa6d26a0f530fc93b5ec5fe50b1f6ad4
alex007sirois <alex007sirois@gmail.com>
2018-03-04 11:58:31 -0500

    #18 controlleur

--- www/action/controlleurConnexion.php
+++ www/action/controlleurConnexion.php
@@ -1,35 +1,57 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/modele/Membre.php';
-
 class controlleurConnexion
 {
     
     public static function isConnecte()
     {
         session_start();
-		return isset($_SESSION["membre"]);
+		return isset($_SESSION["membreConnexion"]);
     }
     
     public static function getMembre()
     {
         session_start();
 		if(self::isConnecte())
-		    return $_SESSION["membre"];
+		    return $_SESSION["membreConnexion"];
 		else
 		    return null;
     }
     
-    public static function connecter()
+    public static function connecter($pseudonyme, $motDePasse)
     {
-        session_start();
-		unset($_SESSION["membre"]);
+        require_once $_SERVER['DOCUMENT_ROOT'].'/dao/MembreDAO.php';
+
+        $membre = new Membre();
+		$membre->setGestionErreur(true);
+		
+		try{
+		    $membre->setPseudonyme($pseudonyme);
+		    $membre->setMotDePasse($motDePasse);
+		    
+		    $membre->hashMotDePasse();
+		    
+		    $membre=MembreDAO::getInstance()->connecterMembre($membre);
+		}
+		catch(Exception $e){
+		    return $e->getMessage();
+		}
+		
+		
+		if(!empty($membre))
+		{
+		    session_start();
+		    $_SESSION["membreConnexion"]=$membre;
+		    return true;
+		}
+		
+		return false;
     }
     
     public static function deconnecter()
     {
         session_start();
-		unset($_SESSION["membre"]);
+		unset($_SESSION["membreConnexion"]);
     }
     
 }
