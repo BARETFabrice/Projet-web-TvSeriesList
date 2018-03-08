e3ebb5fe01f41b82a1a3ee103e5067527ae24818
alex007sirois <alex007sirois@gmail.com>
2018-03-08 11:12:10 -0500

    #27 ajout du controlleur dans la vue

--- www/prive/membre/parametres.php
+++ www/prive/membre/parametres.php
@@ -1,32 +1,31 @@
 <?php
 include "../../public/fragmentHautPage.php";
+require_once $_SERVER['DOCUMENT_ROOT'].'/action/controlleurParametres.php';
+
+$membre=getMembre();
 ?>
 
 <div class="row column align-center medium-6 large-4 container-padded div_login">
-    <form class="log-in-form">
+    <form class="log-in-form" method="post">
       <h4 class="text-center">Modifier parametres du membre</h4>
       
       <hr>
       
       <label>Notifications
-        <input type="checkbox">
+        <input name="notification" type="checkbox" <?php if($membre->isNotification())echo 'checked'; ?>>
       </label>
       
-      <label>Visibilité de votre liste
-        <select name="visibility">
-          <option value="private">Privée</option>
-          <option value="friends">Amis seulement</option>
-          <option value="public">Publique</option>
-        </select>
+      <label>Courriel
+        <input type="email" nom="courriel" value="<?=$membre->getCourriel()?>" disabled>
       </label>
 	  
 	  <label>Changer de nom
-        <input type="text" placeholder="somebody">
+        <input type="text" nom="pseudonyme" value="<?=$membre->getPseudonyme()?>">
       </label>
       
       <label>Changer de Mot de passe
-        <input type="password" placeholder="Old Password">
-        <input type="password" placeholder="New Password">
+        <input name="vieuxMotDePasse" type="password" placeholder="Old Password">
+        <input name="nouveauMotDePasse" type="password" placeholder="New Password">
       </label>
       
       <p><input type="submit" class="button expanded" value="Confirm"></input></p>
