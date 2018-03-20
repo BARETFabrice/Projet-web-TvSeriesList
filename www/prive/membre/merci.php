713627cac2a352a0faa9cdc5bc5a43e9e360efca
alex007sirois <alex007sirois@gmail.com>
2018-03-19 20:51:45 -0400

    #29 remerciment

---
+++ www/prive/membre/merci.php
@@ -0,0 +1,12 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/public/fragmentHautPage.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/action/controlleurDonation.php';
+?>
+
+<h1>MERCI</h1>
+
+
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'].'/public/fragmentBasPage.php';
+?>
