d1335d598b4792885d5309f971d1cb4b58327150
fsimoneau <fsimoneau997@gmail.com>
2018-03-22 20:26:51 -0400

    Ajax dans la page paramètres

--- www/prive/membre/parametres.php
+++ www/prive/membre/parametres.php
@@ -7,6 +7,41 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/action/controlleurParametres.php';
 
 $membre=getMembre();
 ?>
+<script>
+	function ChangerNotification(valeur)
+	{
+		if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("paramNotification").style.color = "Green";
+				setTimeout( function(){document.getElementById("paramNotification").style.color = "Black";}, 1000);
+            }
+        };
+        xmlhttp.open("GET","/action/controlleurParametres.php?notification="+valeur,true);
+        xmlhttp.send();
+	}
+	
+	function ChangerNom(valeur)
+	{
+		if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                 document.getElementById("paramNom").style.color = "Green";
+				setTimeout( function(){document.getElementById("paramNom").style.color = "Black";}, 1000);
+            }
+        };
+        xmlhttp.open("GET","/action/controlleurParametres.php?nom="+valeur,true);
+        xmlhttp.send();
+	}
+</script>
 
 <div class="row column align-center medium-6 large-4 container-padded div_login">
     <form class="log-in-form" method="post">
@@ -14,26 +49,27 @@ $membre=getMembre();
       
       <hr>
       
-      <p class="messageErreure"><?=_($erreur)?></p>
+      <p class="messageErreure"><?=$erreur?></p>
       
-      <label>Notifications
-        <input name="notification" type="checkbox" <?php if($membre->isNotification())echo 'checked'; ?>>
+      <label id="paramNotification">Notifications
+        <input name="notification" onchange="ChangerNotification(this.checked)" type="checkbox" <?php if($membre->isNotification())echo 'checked'; ?>>
       </label>
       
       <label>Courriel
         <input type="email" name="courriel" value="<?=$membre->getCourriel()?>" disabled>
       </label>
 	  
-	  <label>Changer de nom
-        <input type="text" name="pseudonyme" value="<?=$membre->getPseudonyme()?>">
+	  <label id="paramNom">Changer de nom
+        <input type="text" name="pseudonyme" onchange="ChangerNom(this.value)" value="<?=$membre->getPseudonyme()?>">
       </label>
       
-      <label>Changer de Mot de passe
+	   <hr>
+	   
+     <h4 class="text-center">Changer le mot de passe</h4>
         <input name="vieuxMotDePasse" type="password" placeholder="Old Password">
         <input name="motDePasse" type="password" placeholder="New Password">
-      </label>
       
-      <p><input name="submitParametres" type="submit" class="button expanded" value="Confirm"></input></p>
+      <p><input name="submitParametres" type="submit" class="button expanded" value="Changer mot de passe"></input></p>
     </form>
 </div>
 
